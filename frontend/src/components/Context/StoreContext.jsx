e79893edccaace02beb366f4dafd93cfda5b4f91
Ajay07102000 <162734960+Ajay07102000@users.noreply.github.com>
2024-07-23 22:24:11 +0530

    Update StoreContext.jsx

--- frontend/src/components/Context/StoreContext.jsx
+++ frontend/src/components/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext =createContext(null)
 
 const StoreContextProvider =(props)=>{
 
-    const url="https://food-del-backend-ttr4.onrender.com";
+    const url="https://food-del-backend-loit.onrender.com";
     const [cartItems,setCartItems] =useState({});
     const [food_list,setFoodList] =useState([])
     const addToCart =(itemId)=>{
