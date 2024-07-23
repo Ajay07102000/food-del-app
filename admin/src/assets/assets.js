24f9f14c462a352e56dc173afd8c1cf25c2f26d0
Ajay07102000 <162734960+Ajay07102000@users.noreply.github.com>
2024-07-23 22:25:45 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 
-export const url = 'https://food-del-backend-ttr4.onrender.com'
+export const url = 'https://food-del-backend-loit.onrender.com'
 export const assets ={
     logo,
     add_icon,
